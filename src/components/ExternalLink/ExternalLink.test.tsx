7e34328cbea86728d6b930c3bd040b098301a0e7
Andrés Nakanishi <andres@simplenight.com>
2022-07-12 17:36:49 -0300

    [ADD] Added ExternalLink Test

---
+++ src/components/ExternalLink/ExternalLink.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ExternalLink from "./ExternalLink";
+
+describe("Button", () => {
+  test("Renders the external link component", () => {
+    render(
+      <ExternalLink href="#" className="nothing">
+        Falopa
+      </ExternalLink>
+    );
+  });
+});
