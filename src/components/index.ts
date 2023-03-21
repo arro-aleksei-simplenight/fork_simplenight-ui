90512a35e274ed673a7fffdfb78ecf1a1f249c3b
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2023-03-21 11:06:00 -0300

    Remove disclaimer component (#51)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Button } from './Button';
 export { default as ExternalLink } from './ExternalLink';
-export { default as Disclaimer } from './Disclaimer';
 export { default as Heading } from './Heading';
 export { default as Paragraph } from './Paragraph';
 export { default as Modal } from './Modal';
