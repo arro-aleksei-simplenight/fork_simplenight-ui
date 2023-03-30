c8ad921e00927a05e115711b103df2a65ead7df5
Andrés Nakanishi <andres@simplenight.com>
2023-03-30 13:06:05 -0300

    [UPDATE] Fix Error404 & Error500

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,6 +1,6 @@
 export { default as EmptyState } from './EmptyState';
-export { default as Error500 } from './Error404';
-export { default as Error404 } from './Error500';
+export { default as Error404 } from './Error404';
+export { default as Error500 } from './Error500';
 export { default as ImageEmpty } from './ImageEmpty';
 export { default as Itinerary } from './Itinerary';
 export { default as NoContent } from './NoContent';
