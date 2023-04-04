8a331099dbdda0f0a3c8c4776a653830f51cbb19
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2023-04-04 11:39:10 -0300

    Refactor select (#62)

--- src/components/Inputs/Select/Select.tsx
+++ src/components/Inputs/Select/Select.tsx
@@ -1,15 +1,12 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/require-default-props */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useRef, useState, ForwardedRef, forwardRef } from 'react';
 import { useOnOutsideClick } from '@/hooks';
-import { ChevronDown, ChevronUp } from '@/icons/regular';
+import { ChevronUp } from '@/icons/regular';
 
-import {
-  ColorsMap,
-  SelectOption,
-  SelectProps,
-} from '@/components/Inputs/types';
+import { SelectOption, SelectProps } from '@/components/Inputs/types';
 
 export interface SelectSpecificProps extends SelectProps {
   leftPadding?: string;
@@ -17,8 +14,17 @@ export interface SelectSpecificProps extends SelectProps {
   options: SelectOption[];
   defaultValue?: SelectOption;
   searchable?: boolean;
+  error?: boolean;
   // eslint-disable-next-line no-unused-vars
-  onChange: (option: SelectOption) => void;
+  onChange?: (option: SelectOption) => void;
+}
+
+function getStylesBySize(inputSize: 'large' | 'small') {
+  return {
+    height: inputSize === 'small' ? 'h-8' : 'h-11',
+    textSize: inputSize === 'small' ? 'text-sm' : 'text-base',
+    iconSize: inputSize === 'large' ? 'w-5 h-5' : 'w-4 h-4',
+  };
 }
 
 const Select = forwardRef(
@@ -33,121 +39,106 @@ const Select = forwardRef(
       onChange,
       leftPadding = 'pl-0',
       hideArrow = false,
+      error,
       ...rest
     }: SelectSpecificProps,
     ref: ForwardedRef<HTMLInputElement>,
   ) => {
-    const [selectedOption, setSelectedOption] = useState<
-      SelectOption | undefined
-    >(defaultValue);
+    const [selectedOption, setSelectedOption] = useState(defaultValue);
+    const [input, setInput] = useState(defaultValue?.label || '');
     const [open, setOpen] = useState(false);
     const [searchResults, setSearchResults] = useState(options);
-    const isDisabled = state === 'disabled';
-
-    const height = inputSize === 'small' ? 'h-8' : 'h-11';
-    const textSize = inputSize === 'small' ? 'text-sm' : 'text-base';
-    const iconSize = inputSize === 'large' ? 'w-5 h-5' : 'w-4 h-4';
-
-    const idleBorderColor = selectedOption
-      ? 'border-dark-400'
-      : 'border-dark-300';
-    const openBorderColor = 'border-primary-1000';
-    const colors: ColorsMap = {
-      idle: `text-dark-1000 ${open ? openBorderColor : idleBorderColor} `,
-      error: `text-dark-1000   ${
-        open ? openBorderColor : 'border-error-1000'
-      } `,
-      success: `text-dark-1000  ${
-        open ? openBorderColor : 'border-green-1000'
-      }`,
-      disabled: 'text-dark-600 border-dark-300 bg-dark-200',
-    };
-
+    const { height, textSize, iconSize } = getStylesBySize(inputSize);
     const searchInputRef = useRef<HTMLInputElement>(null);
     const selectRef = useRef<HTMLInputElement>(null);
     useOnOutsideClick(selectRef, () => setOpen(false));
-
-    const handleOpenClose = () => {
-      const wasOpen = open;
-      setOpen(!open);
-      if (searchable && !wasOpen) searchInputRef?.current?.focus();
-      if (searchable && wasOpen) searchInputRef?.current?.blur();
-    };
-
-    const handleChange = (option: SelectOption) => {
-      setSelectedOption(option);
-      setOpen(false);
-      onChange(option);
-    };
-
-    const handleSearch = (e: any) => {
-      setSelectedOption(e.target.value);
-      setOpen(true);
-
-      setSearchResults(
-        options.filter((option: SelectOption) => {
-          const matchValue = option.value
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-          const matchLabel = option.label
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-          return matchValue || matchLabel;
-        }),
-      );
-    };
+    /// COLORS:
+    const idleBorderColor = selectedOption
+      ? 'border-dark-400'
+      : 'border-dark-300';
+    const errorColors = `text-dark-1000 ${
+      open ? 'border-primary-1000' : 'border-error-1000'
+    } `;
+    const disabledColors = 'text-dark-600 border-dark-300 bg-dark-200';
+    const inputColors = `text-dark-1000 ${
+      open ? 'border-primary-1000' : idleBorderColor
+    }
+      ${error && errorColors}
+      ${rest.disabled && disabledColors}`;
 
     return (
       <section ref={selectRef}>
         <section
-          className={`flex items-center  w-full border pl-3 pr-2  ${height}  ${
-            colors[state]
-          }
-        ${open ? 'rounded-t' : 'rounded'}
-        `}
-          onClick={!isDisabled ? handleOpenClose : undefined}
+          ref={searchInputRef}
+          className={`flex items-center w-full border pl-3 pr-2 rounded ${height} ${inputColors} ${
+            open && 'rounded-t'
+          }`}
+          onClick={(e) => {
+            if (rest.disabled || !searchInputRef.current) {
+              return;
+            }
+            e.preventDefault();
+            e.stopPropagation();
+            if (!searchable || (searchable && !open)) setOpen((o) => !o);
+            if (searchable && !open) searchInputRef.current.focus();
+            if (searchable && open) searchInputRef.current.blur();
+          }}
         >
           <input
             type="text"
-            className={` ${leftPadding} ${textSize} ${colors[state]} ${
+            className={`${leftPadding} ${textSize} ${inputColors} ${
               !searchable && 'cursor-pointer'
             } w-full h-full  border-none focus:shadow-none focus:inset-0 focus:ring-0 focus:outline-none focus:border-transparent`}
             ref={ref}
             placeholder={placeholder}
-            value={selectedOption?.label}
-            onChange={handleSearch}
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+              setSelectedOption(undefined);
+              setOpen(true);
+              setSearchResults(
+                options.filter((option) =>
+                  option.label
+                    .toLowerCase()
+                    .includes(e.target.value.toLowerCase()),
+                ),
+              );
+            }}
             readOnly={!searchable}
             {...rest}
           />
           {!hideArrow && (
             <button type="button">
-              {open ? (
-                <ChevronUp className={`${iconSize} text-dark-700`} />
-              ) : (
-                <ChevronDown className={`${iconSize} text-dark-700`} />
-              )}
+              <ChevronUp
+                className={`text-dark-700 transition-all ${iconSize} ${
+                  !open && 'rotate-180'
+                }`}
+              />
             </button>
           )}
         </section>
         <section
-          className={`${!open ? 'hidden ' : ''} border ${
-            colors[state]
-          }  border-t-0 rounded-b `}
+          className={`border border-t-0 rounded-b ${inputColors} ${
+            !open ? 'hidden' : 'block'
+          }`}
         >
           {searchResults.map((option: SelectOption) => (
-            <div
-              key={`${option.value}`}
-              onClick={() => handleChange(option)}
-              className="p-2 border-b border-solid cursor-pointer select-none border-dark-200 hover:bg-dark-100 last:border-b-0 "
+            <button
+              key={option.value}
+              onClick={() => {
+                setSelectedOption(option);
+                setInput(option.label);
+                setOpen(false);
+                onChange?.(option);
+              }}
+              className="block w-full p-2 border-b border-solid cursor-pointer select-none border-dark-200 hover:bg-dark-100 last:border-b-0"
             >
               {option.label}
-            </div>
+            </button>
           ))}
         </section>
       </section>
     );
   },
 );
-
-Select.defaultProps = { leftPadding: 'pl-0', hideArrow: false };
 export default Select;
